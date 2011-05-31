2fb977e48778e6b85f02a3de8bc0c647d964ecc2
Niko Felger <niko.felger@gmail.com>
2011-05-31 20:26:16 +0100

    Can create users at /users/new. Problem: for FBC you really need a username/password combo to log in as the test user. FB's calls only return this on the request that creates the user. Need to see if there's a way to get user/pw post-fact.

--- lib/app.rb
+++ lib/app.rb
@@ -3,11 +3,31 @@ require 'curb-fu'
 require 'json'
 
 get '/users' do
-  CurbFu.get("https://graph.facebook.com/#{credentials["app_id"]}/accounts/test-users?access_token=#{access_token}").body
+  CurbFu.get("https://graph.facebook.com/#{app_id}/accounts/test-users?access_token=#{access_token}").body
+end
+
+get '/users/new' do
+  erb :"users/new"
+end
+
+post '/users/new' do
+  CurbFu.get("https://graph.facebook.com/#{app_id}/accounts/test-users?" +
+    "installed=#{params["installed"] ? "true" : "false"}" +
+    "&permissions=#{params["permissions"]}" +
+    "&method=post" +
+    "&access_token=#{access_token}").body
 end
 
 def access_token
-  /access_token=(.*)/.match(CurbFu.get("https://graph.facebook.com/oauth/access_token?client_id=#{credentials["app_id"]}&client_secret=#{credentials["app_secret"]}&grant_type=client_credentials").body)[1]
+  @access_token ||= /access_token=(.*)/.match(CurbFu.get("https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&client_secret=#{app_secret}&grant_type=client_credentials").body)[1]
+end
+
+def app_id
+  credentials["app_id"]
+end
+
+def app_secret
+  credentials["app_secret"]
 end
 
 def credentials
